9c2e8a3071a15500a2db326e668d8134dbfc2283
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-02-06 18:26:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,34 +12,41 @@ app.use(require('body-parser').json({
 }));
 (async () => {
     app.post('/mail_order', async (req, res) => {
-        var transporter = nodemailer.createTransport(
-            {
-                host: process.env.TRANSPORT_HOST,
-                port: process.env.TRANSPORT_PORT,
-                secure: true,
-                auth: {
-                    user: process.env.TRANSPORT_USER,
-                    pass: process.env.TRANSPORT_PWD,
-                },
+        try {
+            var transporter = nodemailer.createTransport(
+                {
+                    host: process.env.TRANSPORT_HOST,
+                    port: process.env.TRANSPORT_PORT,
+                    secure: true,
+                    auth: {
+                        user: process.env.TRANSPORT_USER,
+                        pass: process.env.TRANSPORT_PWD,
+                    },
+                });
+
+            var mailOptions = {
+                from: process.env.TRANSPORT_USER,
+                to: process.env.MAILTO_USER,
+                subject: '来自爱发电的消息',
+                text: JSON.stringify(req.body),
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
             });
+            res.status(200).json({ "ec": 200 })
 
-        var mailOptions = {
-            from: process.env.TRANSPORT_USER,
-            to: process.env.MAILTO_USER,
-            subject: '来自爱发电的消息',
-            text: JSON.stringify(req.body),
-        };
+        } catch (e) {
+            res.status(500).json({
+                message: e.message,
+                stack: e.stack
+            })
+        }
 
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
-        res.status(200).json({"ec":200} )
     })
     const server = app.listen(~~process.env.FC_SERVER_PORT || 15111)
-    server.timeout = 0
-    server.keepAliveTimeout = 0
 })()
